0d221c473547629877496c2fd7897905dfada3c5
V1t0r_HuHn <104599280+VitorHuHn@users.noreply.github.com>
2026-06-26 22:37:35 -0300

    Add files via upload

---
+++ Ex_7/Ex_7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vezes;
+            int multi;
+            string a;
+            string b;
+
+            vezes = 0;
+
+            Console.Write("Aperte para Iniciar a tabuada!");
+            a = (Console.ReadLine());
+
+            do
+            {
+                vezes = vezes + 1;
+                multi = 10 * vezes;
+                Console.WriteLine("20 x {0} : {1}", vezes, multi);
+
+            }
+            while (vezes < 10);
+            Console.Write("Aperte agora para ver a segunda parte!");
+            b = (Console.ReadLine());
+            do
+            {
+                vezes = vezes + 1;
+                multi = 20 * vezes;
+                Console.WriteLine("20 x {0} : {1}", vezes, multi);
+
+            }
+            while (vezes < 20);
+        }
+    }
+}
---
+++ Ex_8/Ex_8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+
+            for (int i = 1; i <= 100; i++) {
+                soma += i;
+            }
+            Console.WriteLine("A Soma dos inteiros de 1 a 100 é: {0}", soma);
+        }
+    }
+}
